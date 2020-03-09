0a62b0110fc9df05a6804dd88c7ed57bfafdc909
liaoquewuhen <651753474@qq.com>
2020-03-09 13:14:25 +0800

    增加了html格式引入图片的路径修改

--- md小工具/图片路径修改/md_image.py
+++ md小工具/图片路径修改/md_image.py
@@ -37,6 +37,17 @@ for i in range(len(files_name)):
 				if path[0] != '.':
 					shutil.copy2(thisline,new_path)
 					#print('将文件复制到:',new_path)
+			if('<img' in lines[j]):                                   #html格式插入的图片
+				first = lines[j].find('\"')
+				next = lines[j].find("\"",first+1)
+				thisline = lines[j][first+1:next]                 #取出这一行的图片路径	
+				#print('原图片存放在：',thisline)
+				path,name = os.path.split(thisline)
+				new_path = './'+folder_name+'/' + name                    #这里默认存在image目录，TODO
+				lines[j] = lines[j].replace(thisline,new_path)  #替换md笔记里路径				
+				if path[0] != '.':
+					shutil.copy2(thisline,new_path)
+					#print('将文件复制到:',new_path)
 			file_date = file_date + lines[j]                    #存储新的文件内容
 	with open(files_name[i],'w',encoding = 'UTF-8') as f:	
 		f.write(file_date)
