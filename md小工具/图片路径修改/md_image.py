47a3539ee02f6410e466b1f0c584522aa4df70bb
liaoquewuhen <651753474@qq.com>
2020-03-14 13:58:07 +0800

    增加了修改别处目录下md笔记的功能

--- md小工具/图片路径修改/md_image.py
+++ md小工具/图片路径修改/md_image.py
@@ -3,7 +3,20 @@ import shutil
 import re
 
 
-files_name = [x for x in os.listdir() if '.md' in x]            #取出当前目录下面所有md文件
+noExist = True
+
+while noExist:
+	fileway = input("输入一个路径")
+	try:
+		os.listdir(fileway)
+	except FileNotFoundError:
+		print('请输入正确的路径')
+		continue
+	noExist = False
+
+
+
+files_name = [x for x in os.listdir(fileway) if '.md' in x]            #取出当前目录下面所有md文件
 sep_here = os.sep
 folder_name = input("可输入本地存在的文件夹名以存放图片:（默认为image）")
 find_folder = False
@@ -11,7 +24,7 @@ find_folder = False
 while find_folder == False:
 	if folder_name == "":
 		folder_name = "image"
-	if folder_name not in os.listdir():
+	if folder_name not in os.listdir(fileway):
 		chocie = input("文件夹不存在，是否直接创建文件夹[y]/n")
 		if chocie == "" or chocie == "y":
 			os.mkdir(folder_name)
@@ -24,33 +37,34 @@ while find_folder == False:
 for i in range(len(files_name)):
 	file_date = ""
 	print("正在修改文件：",files_name[i])
-	with open(files_name[i],'r',encoding = 'UTF-8') as f:
+	with open(fileway+'/'+files_name[i],'r',encoding = 'UTF-8') as f:
 		lines = f.readlines()		
 		for j in range(len(lines)):
 			if('![' in lines[j]):
 				first = lines[j].find('(')
 				next = lines[j].find(')')
 				thisline = lines[j][first+1:next]                 #取出这一行的图片路径	
-				#print('原图片存放在：',thisline)
+				print('原图片存放在：',thisline)
 				path,name = os.path.split(thisline)
 				new_path = './'+folder_name+'/' + name                    #这里默认存在image目录，TODO
+				abs_path = fileway + '/'+folder_name+'/' + name
 				lines[j] = lines[j].replace(thisline,new_path)  #替换md笔记里路径				
 				if path[0] != '.':
-					shutil.copy2(thisline,new_path)
-					#print('将文件复制到:',new_path)
+					shutil.copy2(thisline,abs_path)
+					print('将文件复制到:',new_path)
 			if('<img' in lines[j]):                                   #html格式插入的图片
 				first = lines[j].find('\"')
 				next = lines[j].find("\"",first+1)
 				thisline = lines[j][first+1:next]                 #取出这一行的图片路径	
-				#print('原图片存放在：',thisline)
+				print('原图片存放在：',thisline)
 				path,name = os.path.split(thisline)
 				new_path = './'+folder_name+'/' + name                    #这里默认存在image目录，TODO
 				lines[j] = lines[j].replace(thisline,new_path)  #替换md笔记里路径				
 				if path[0] != '.':
 					shutil.copy2(thisline,new_path)
-					#print('将文件复制到:',new_path)
+					print('将文件复制到:',new_path)
 			file_date = file_date + lines[j]                    #存储新的文件内容
-	with open(files_name[i],'w',encoding = 'UTF-8') as f:	
+	with open(fileway+'/'+files_name[i],'w',encoding = 'UTF-8') as f:	
 		f.write(file_date)
 
 
